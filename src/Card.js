7c7dba7ccb677ea078e753cfb11e85f412783981
Jaime Segura <jseguraweb@gmail.com>
2020-02-23 15:20:04 +0100

    pass backgroundimage as a prop to the card component

--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,7 @@ const Card = props => {
     return (
         <div className="card">
             <h2 className="title">{props.title}</h2>
-            <div className="picture"></div>
+            <div className="picture" style={{ backgroundImage: `url(${props.image})` }}></div>
             <div className="box">
                 <p className="rating">Rating: <span>{props.rating}</span></p>
                 <p className="year">Year: <span>{props.year}</span></p>
