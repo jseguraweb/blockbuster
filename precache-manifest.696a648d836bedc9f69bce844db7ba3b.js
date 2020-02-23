1ee40c5982e9f3327b78805e6e2048ce4f48d536
Jaime Segura <jseguraweb@gmail.com>
2020-02-23 23:52:05 +0100

    Updates

---
+++ precache-manifest.696a648d836bedc9f69bce844db7ba3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c839a68848605609314977e721c96f1",
+    "url": "/blockbuster/index.html"
+  },
+  {
+    "revision": "e3f30959bbd9a1bd8a59",
+    "url": "/blockbuster/static/css/main.003a8ec6.chunk.css"
+  },
+  {
+    "revision": "855d6e93a009443b0991",
+    "url": "/blockbuster/static/js/2.0c1a8041.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/blockbuster/static/js/2.0c1a8041.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3f30959bbd9a1bd8a59",
+    "url": "/blockbuster/static/js/main.3459d1b2.chunk.js"
+  },
+  {
+    "revision": "17a64669d925e48c82c7",
+    "url": "/blockbuster/static/js/runtime-main.1f959007.js"
+  }
+]);
