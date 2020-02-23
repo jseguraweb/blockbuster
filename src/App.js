26317de55aa4b002f4c2a64d987763531c149ba7
Jaime Segura <jseguraweb@gmail.com>
2020-02-23 23:28:09 +0100

    add filter method

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ export default class App extends Component {
   state = {
     movies: [],
     loading: true,
-    displayError: false
+    displayError: false,
+    search: ''
   }
 
   componentDidMount = async () => {
@@ -17,26 +18,42 @@ export default class App extends Component {
       const serverData = await fetch('https://yts.mx/api/v2/list_movies.json');
       const response = await serverData.json();
       this.setState({ movies: response.data.movies });
-      console.log(this.state.movies);
+      // console.log(this.state.movies);
     } catch (err) {
       this.setState({ displayError: 'ERROR: Something seems to be wrong' })
     }
     this.setState({ loading: false });
   }
 
+  filterMovies = event => {
+    this.setState({ search: (event.target.value) });
+    // console.log(this.state.search);
+  }
+
   render() {
+    // display loading while waiting for fetching the data
     if (this.state.loading === true) {
       return <Loading />
-    } else if (this.state.displayError !== false) {
+    }
+    // display an error if the data can't be fetched
+    else if (this.state.displayError !== false) {
       return <Error displayError={this.state.displayError} />
     }
+    // filtering movies: indexOf whatever is in this.state.search and if it's not -1 (which would mean that it doesn' exist)
+    let filteredMovies = this.state.movies.filter(movie => movie.title.toLocaleLowerCase().indexOf(this.state.search.toLocaleLowerCase()) !== -1);
 
     return (
       <div className="container">
         <h1>Blockbuster</h1>
+        <form action="#">
+          <label htmlFor="films">
+            <span>Filter</span>
+            <input type="text" id="films" onChange={this.filterMovies} />
+          </label>
+        </form>
         <div className="wrapper">
           {
-            this.state.movies.map((movie, index) => {
+            filteredMovies.map((movie, index) => {
               return <Card key={index} title={movie.title} image={movie.medium_cover_image} rating={movie.rating} year={movie.year} />
             })
           }
